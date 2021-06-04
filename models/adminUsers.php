d391a7ced1e90bc34e21a713da089448797477e7
Ilya Shabalin <lomtvador@gmail.com>
2021-06-04 20:57:53 +0300

    Исправлен баг с изменением логина

--- models/adminUsers.php
+++ models/adminUsers.php
@@ -47,17 +47,20 @@ class Model
         $this->db = new Database();
         try {
             $this->db->mysqli->begin_transaction();
-            $sql = 'SELECT login FROM user WHERE login = ?';
+            $sql = 'SELECT id, login FROM user WHERE login = ?';
             $stmt = $this->db->mysqli->prepare($sql);
             $stmt->bind_param('s', $obj['login']);
             $stmt->execute();
             $result = $stmt->get_result();
             $stmt->close();
             if ($result->num_rows > 0) {
-                $login = $obj['login'];
-                $this->db->mysqli->rollback();
-                $this->db->mysqli->close();
-                new Message("Логин $login уже занят.");
+                $row = $result->fetch_assoc();
+                if (intval($row['id']) !== intval($obj['id'])) {
+                    $login = $obj['login'];
+                    $this->db->mysqli->rollback();
+                    $this->db->mysqli->close();
+                    new Message("Логин $login уже занят.");
+                }
             }
             $stmt = $this->db->mysqli->prepare('SELECT * FROM user WHERE id = ?');
             $stmt->bind_param('i', $obj['id']);
